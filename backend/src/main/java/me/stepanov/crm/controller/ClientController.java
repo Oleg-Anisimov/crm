574a7071c3eb77a5eab49e6a9a9cac3c1cae77fe
Lemain99 <102553300+Lemain99@users.noreply.github.com>
2023-02-18 14:55:11 +0300

    Add client's service and controller

--- backend/src/main/java/me/stepanov/crm/controller/ClientController.java
+++ backend/src/main/java/me/stepanov/crm/controller/ClientController.java
@@ -1,10 +1,13 @@
 package me.stepanov.crm.controller;
 
+import me.stepanov.crm.domain.Client;
 import me.stepanov.crm.dto.ClientDto;
 import me.stepanov.crm.service.ClientService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/client")
@@ -23,4 +26,22 @@ public class ClientController {
         clientService.delete(id);
         return ResponseEntity.ok().build();
     }
+
+    @PostMapping("/update")
+    public ResponseEntity update(@RequestBody Client client){
+        clientService.update(client);
+        return ResponseEntity.ok().build();
+    }
+
+    @GetMapping("/byId")
+    public Client getById(Long id){
+        return clientService.getById(id);
+    }
+
+    @GetMapping("/byAll")
+    public List<ClientDto> getAll(){
+        return clientService.getAll();
+    }
+
+
 }
